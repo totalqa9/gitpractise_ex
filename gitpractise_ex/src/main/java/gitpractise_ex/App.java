fc0904546da90bd2f417df37436c45435248864c
totalqa <totalqa9@gmail.com>
2020-09-16 05:37:40 +0530

    Pushing the code to master

---
+++ gitpractise_ex/src/main/java/gitpractise_ex/App.java
@@ -0,0 +1,13 @@
+package gitpractise_ex;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
